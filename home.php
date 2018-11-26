acf230f37e11495527cb9b0296681da6e3b2a49c
Sonica Gupta <guptaso@flip2.engr.oregonstate.edu>
2018-11-25 16:57:08 -0800

    Starting ...

---
+++ home.php
@@ -0,0 +1,10 @@
+<html>
+<header>
+  <h1> Home Page </h1>
+</header>
+<body>
+  <p> Create Account (not created yet...)</p>
+  <a href="./search_flights.php"> Search Flights </a>
+
+</body>
+</html>
---
+++ search_flights.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+  <h1> Seach Flights </h1>
+
+  <div id="searchBar" class="searchBar">
+    <h2 id="searchBarHead"> Enter Search Criteria </h2>
+    <form name = "fill-in" id="fill-in">
+      <table id="UserFillTable">
+        <tr>
+          <td><label for="field_one">Name</label></td>
+          <td><input id="field_one" name="field_one" type="text"> </td>
+        </tr>
+      </table>
+    </form>
+  </div>
+
+
+</body>
+</html>
