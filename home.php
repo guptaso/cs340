1b22576334482a0791b62627fcdce825e375915c
Lauren Sunamoto <sunamotl@flip2.engr.oregonstate.edu>
2018-11-27 23:10:09 -0800

    add link

--- home.php
+++ home.php
@@ -5,12 +5,11 @@
 	<link rel="stylesheet" type="text/css" href="style.css"> 
 </head>
 <body style="background-image:url(http://web.engr.oregonstate.edu/~sunamotl/braden-jarvis-383867-unsplash.jpg)">
-	<div class="header">
 	<div class="nav-acc">
 		<a href="./myacc.php">My Account</a>
 		<a href="./create_account.php">Create Account</a>
 		<a href="./search_flights.php">Search Flights</a>
-	</div>
+		<a href="./purchase.php">Purchase Flight</a>
 	</div>
 	<div class="center">
 		<h1>Search and book flights with Flight Scheduler. We're here to get you there.</h1>
